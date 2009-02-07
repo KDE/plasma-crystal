9dfaf25bbb73cf31dc0d4ea925fa4c3dbe6e0b9b
Richard J. Moore <rich@kde.org>
2009-02-07 11:19:00 +0000

    - Improve the mediawiki api. You can now set a timeout and cancel   requests.
    - Now autodetects the base of the wiki correctly so it can form urls as
      well as just page titles.
    
    
    svn path=/trunk/playground/base/plasma/applets/crystal/; revision=922598

--- mediawiki.cpp
+++ mediawiki.cpp
@@ -1,26 +1,60 @@
+/*
+ *   Copyright 2009 by Richard Moore <rich@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
-#include <QStringList>
 #include <QXmlStreamReader>
+#include <QTimer>
 #include <QDebug>
 
 #include "mediawiki.h"
 
+enum State {
+    StateApiChanged,
+    StateApiUpdating,
+    StateReady
+};
+
 struct MediaWikiPrivate {
-    QHash<QString, QUrl> results;
+    int state;
+    QList<MediaWiki::Result> results;
     QUrl apiUrl;
+    QUrl baseUrl;
     QNetworkAccessManager *manager;
     int maxItems;
+    QNetworkReply *reply;
+    int timeout;
+    QUrl query;
 };
 
 MediaWiki::MediaWiki( QObject *parent )
         : QObject( parent )
 {
     d = new MediaWikiPrivate;
+    d->state = StateApiChanged;
     d->apiUrl = QUrl("http://en.wikipedia.org/w/api.php");
     d->manager = new QNetworkAccessManager( this );
     d->maxItems = 10;
+    d->timeout = 30 * 1000; // 30 second
+    d->reply = 0;
+
     connect( d->manager, SIGNAL(finished(QNetworkReply*)), SLOT(finished(QNetworkReply *)) );
 }
 
@@ -29,7 +63,7 @@ MediaWiki::~MediaWiki()
     delete d;
 }
 
-QHash<QString, QUrl> MediaWiki::results() const
+QList<MediaWiki::Result> MediaWiki::results() const
 {
     return d->results;
 }
@@ -51,9 +85,31 @@ QUrl MediaWiki::apiUrl() const
 
 void MediaWiki::setApiUrl( const QUrl &url )
 {
+    if ( d->apiUrl == url )
+	return;
+
     d->apiUrl = url;
+    d->state = StateApiChanged;
 }
 
+int MediaWiki::timeout() const
+{
+    return d->timeout;
+}
+
+void MediaWiki::setTimeout( int millis )
+{
+    d->timeout = millis;
+}
+
+void MediaWiki::abort()
+{
+    if ( !d->reply )
+	return;
+
+    d->reply->abort();
+    d->reply = 0;
+}
 
 void MediaWiki::search( const QString &searchTerm )
 {
@@ -66,7 +122,27 @@ void MediaWiki::search( const QString &searchTerm )
 
     qDebug() << "Constructed search URL" << url;
 
-    d->manager->get( QNetworkRequest(url) );
+    if ( d->state == StateReady ) {
+	d->reply = d->manager->get( QNetworkRequest(url) );
+	QTimer::singleShot( d->timeout, this, SLOT( abort() ) );
+    }
+    else if ( d->state == StateApiChanged ) {
+	d->query = url;
+	findBase();
+    }
+}
+
+void MediaWiki::findBase()
+{
+    // http://en.wikipedia.org/w/api.php?action=query&meta=siteinfo
+    QUrl url = d->apiUrl;
+    url.addQueryItem( QString("action"), QString("query") );
+    url.addQueryItem( QString("format"), QString("xml") );
+    url.addQueryItem( QString("meta"), QString("siteinfo") );
+
+    qDebug() << "Constructed base query URL" << url;
+    d->reply = d->manager->get( QNetworkRequest(url) );
+    d->state = StateApiUpdating;
 }
 
 void MediaWiki::finished( QNetworkReply *reply )
@@ -78,8 +154,46 @@ void MediaWiki::finished( QNetworkReply *reply )
     }
 
     qDebug() << "Request succeeded";
-    bool ok = processSearchResult( reply );
-    emit finished( ok );
+
+    if ( d->state == StateApiUpdating ) {
+	bool ok = processBaseResult( reply );
+	emit finished( ok );
+	reply->deleteLater();
+
+	d->state = StateReady;
+	d->reply = d->manager->get( QNetworkRequest(d->query) );
+	QTimer::singleShot( d->timeout, this, SLOT( abort() ) );
+    }
+    else {
+	bool ok = processSearchResult( reply );
+
+	emit finished( ok );
+	d->reply->deleteLater();
+	d->reply = 0;
+    }
+}
+
+bool MediaWiki::processBaseResult( QIODevice *source )
+{
+    QXmlStreamReader reader( source );
+
+    while ( !reader.atEnd() ) {
+        QXmlStreamReader::TokenType tokenType = reader.readNext();
+        // qDebug() << "Token" << int(tokenType);
+        if ( tokenType == QXmlStreamReader::StartElement ) {
+            if ( reader.name() == QString("general") ) {
+                QXmlStreamAttributes attrs = reader.attributes();
+
+		d->baseUrl = QUrl( attrs.value( QString("base") ).toString() );
+		return true;
+	    }
+	}
+        else if ( tokenType == QXmlStreamReader::Invalid )
+            return false;
+
+    }
+
+    return true;
 }
 
 bool MediaWiki::processSearchResult( QIODevice *source )
@@ -93,19 +207,21 @@ bool MediaWiki::processSearchResult( QIODevice *source )
         if ( tokenType == QXmlStreamReader::StartElement ) {
             if ( reader.name() == QString("p") ) {
                 QXmlStreamAttributes attrs = reader.attributes();
-                //qDebug() << "Found page" << attrs.value( QString("title") );
-                QString base = d->apiUrl.toString(QUrl::RemovePath);
-                base.append("/");
-                QString path = attrs.value( QString("title") ).toString();
-                base.append(path);
-                QUrl url = QUrl(base);
-                d->results[attrs.value( QString("title") ).toString()] = url;
+
+		Result r;
+		r.url = d->baseUrl.resolved( attrs.value( QString("title") ).toString() );
+		r.title = attrs.value( QString("title") ).toString();
+
+		qDebug() << "Got result: url=" << r.url << "title=" << r.title;
+
+		d->results.append( r );
             }
         }
         else if ( tokenType == QXmlStreamReader::Invalid )
             return false;
     }
 
-    qDebug() << "Results" << d->results;
     return true;
 }
+
+
--- mediawiki.h
+++ mediawiki.h
@@ -1,8 +1,27 @@
+/*
+ *   Copyright 2009 by Richard Moore <rich@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #ifndef MEDIAWIKI_H
 #define MEDIAWIKI_H
 
 #include <QObject>
-#include <QStringList>
+#include <QList>
 #include <QUrl>
 
 class QNetworkReply;
@@ -12,10 +31,31 @@ class MediaWiki : public QObject
     Q_OBJECT
 
 public:
+    class Result {
+    public:
+	Result() {
+	};
+
+	Result( const Result &r ) {
+	    this->title = r.title;
+	    this->url = r.url;
+	};
+
+	Result &operator= (const Result &other)  {
+	    this->title = other.title;
+	    this->url = other.url;
+
+	    return *this;
+	};
+
+	QString title;
+	QUrl url;	
+    };
+
     MediaWiki( QObject *parent );
     virtual ~MediaWiki();
 
-    QHash <QString, QUrl> results() const;
+    QList<MediaWiki::Result> results() const;
 
     int maxItems() const;
     void setMaxItems( int limit );
@@ -23,16 +63,23 @@ public:
     QUrl apiUrl() const;
     void setApiUrl( const QUrl &url );
 
+    int timeout() const;
+    void setTimeout( int millis );
+
 signals:
     void finished( bool success );
 
 public slots:
     void search( const QString &searchTerm );
 
+    void abort();
+
 private slots:
     void finished( QNetworkReply *reply );
 
 private:
+    void findBase();
+    bool processBaseResult( QIODevice *source );
     bool processSearchResult( QIODevice *source );
 
     struct MediaWikiPrivate *d;
